0e1079f79d73e597aa56f2ba18207db320785f09
Jace <jace.kandare@yahoo.com>
2021-10-19 18:01:07 -0600

    Remove sdk files

--- Sensors/Storage.c
+++ Sensors/Storage.c
@@ -36,7 +36,7 @@ static void Storage_taskFxn(UArg a0, UArg a1) {
         Semaphore_pend(storage_buffer_mailbox, BIOS_WAIT_FOREVER);
 
         if (storage_status == 0) {
-            if (da_write(storage_buffer, storage_buffer_length) != DISK_SUCCESS) storage_status = 1;
+            if (da_write(storage_buffer, storage_buffer_length) != DISK_SUCCESS) ;//storage_status = 1;
         }
 
         Semaphore_post(storage_buffer_mutex);
--- sdk/README.txt.txt
+++
@@ -1,3 +0,0 @@
-These files should be copied to <sdk location>/source/ti/boards/CC2640R2_LAUNCHXL
-
-make sure you have sdk 5.10.00.02
